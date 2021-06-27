77e69f6c40de047cd11c770bd318fb55ae4ced20
FKGReborn <37043021+FKGReborn@users.noreply.github.com>
2021-06-27 13:48:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sstp (Wegare mod fakegrafis for linux)
 printf 'ctrl+c' | crontab -e > /dev/null
-apt update && apt install sstp-client pptpd fping libevent-dev libssl-dev
+apt update && apt install sstp-client pptpd fping libevent-dev libssl-dev figlet
 wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/sstp/main/sstp.sh" -O /usr/bin/sstp
 wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/sstp/main/autorekonek-sstp.sh" -O /usr/bin/autorekonek-sstp
 chmod +x /usr/bin/sstp
