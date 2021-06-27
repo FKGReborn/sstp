ca40454eeadf0666a05fca6bd4b2feea7f20e11d
FKGReborn <37043021+FKGReborn@users.noreply.github.com>
2021-06-27 08:55:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #sstp (Wegare)
 printf 'ctrl+c' | crontab -e > /dev/null
-apt update && apt install sstp-client pptpd fping libevent-dev libssl-dev
+apt update && apt install pptpd fping libevent-dev libssl-dev
 wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/sstp/main/sstp.sh" -O /usr/bin/sstp
 wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/sstp/main/autorekonek-sstp.sh" -O /usr/bin/autorekonek-sstp
 chmod +x /usr/bin/sstp
