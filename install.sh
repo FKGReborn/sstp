34379c63bc27918ce4f0f11bca6f521d3f61d398
FKGReborn <37043021+FKGReborn@users.noreply.github.com>
2021-06-27 08:52:25 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#sstp (Wegare)
+printf 'ctrl+c' | crontab -e > /dev/null
+apt update && apt install sstp-client pptpd fping libevent-dev libssl-dev
+wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/sstp/main/sstp.sh" -O /usr/bin/sstp
+wget --no-check-certificate "https://raw.githubusercontent.com/FKGReborn/sstp/main/autorekonek-sstp.sh" -O /usr/bin/autorekonek-sstp
+chmod +x /usr/bin/sstp
+chmod +x /usr/bin/autorekonek-sstp
+rm -r ~/install.sh
+mkdir -p ~/akun/
+touch ~/akun/sstp.txt
+sleep 2
+echo "install selesai"
+echo "untuk memulai tools silahkan jalankan perintah 'sstp'"
+				
